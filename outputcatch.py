b9d2039e6771111262a327dbe575ab6add4d69b7
Lucasljungberg <lucasljungberg@gmail.com>
2015-11-09 13:20:25 +0100

    Updates to file-reading

--- outputcatch.py
+++ outputcatch.py
@@ -1,17 +1,17 @@
 import subprocess
 import os
 import os.path
-for file in os.listdir(""):
+for file in os.listdir("."):
     if file.endswith(".in"):
-        output = subprocess.getoutput("java -jar test.jar " + file)
+        output = subprocess.getoutput("java -jar test.jar < " + file)
         expected_answer = ""
-        with open(file[:-3] + "ans", 'r') as answerfile:
-            expected_answer = answerfile.read()
+        with open(file[:-2] + "ans", 'r') as answerfile:
+        	expected_answer = answerfile.read()
 
         if output == expected_answer:
-            print(file, "succeeded")
+            print(file[:-3], "succeeded")
         else:
-            print(file, "failed")
+            print(file[:-3], "failed")
 
 
 
