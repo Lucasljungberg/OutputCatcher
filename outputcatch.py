4bbeade12530b7c61a4da82e364a656c5690ca40
Lucasljungberg <lucasljungberg@gmail.com>
2015-11-09 15:18:29 +0100

    Functional version

--- outputcatch.py
+++ outputcatch.py
@@ -1,17 +1,26 @@
 import subprocess
 import os
 import os.path
+
+counter = 0
+succeeded = 0
+failed = 0
 for file in os.listdir("."):
     if file.endswith(".in"):
-        output = subprocess.getoutput("java -jar test.jar < " + file)
-        expected_answer = ""
-        with open(file[:-2] + "ans", 'r') as answerfile:
-        	expected_answer = answerfile.read()
+        counter += 1
+        print(" ## Starting %s ##\n" %file)
+        output = subprocess.getoutput("java Main < " + file).strip(' \n').replace(' \n', '\n')
+        print("Output:\n%s" %output)
+        ans_content = ""
+        with open(file[:-2] + "ans", 'r', encoding="utf-8") as answerfile:
+        	ans_content = answerfile.read().strip(' \n').replace(' \n', '\n')
+        print("\nAnswer:\n%s" %ans_content)
 
-        if output == expected_answer:
-            print(file[:-3], "succeeded")
+        if ans_content == output:
+            print("%s succeeded" %file[:-2])
+            succeeded += 1
         else:
-            print(file[:-3], "failed")
+            print("%s failed" %file[:-2])
+            failed += 1
 
-
-
+print("\n%d tests. %d succeeded. %d failed" %(counter, succeeded, failed))
